1a91d5f51302ee6ff0cc43845c8daeace73810dd
brandon <bcooper085@gmail.com>
2017-03-03 14:48:22 -0800

    add js and css, view files including shared layout, index. Forms for add, edit, and delete venue, forms not functional.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,7 +10,22 @@ namespace BandTracker
 
         public HomeModule()
         {
+            Get["/"] = _ => {
+                List<Venue> AllVenues = Venue.GetVenues();
+                return View["index.cshtml", AllVenues];
+            };
 
+            Get["/band/new"] = _ => {
+                return View["bands_venues.cshtml"];
+            };
+
+            Post["/"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                return View["index.cshtml", Venue.GetVenues()];
+            };
+
+            
         }
 
     }
